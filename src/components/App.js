6d07b57d9049dbeac96959a6b98e61e5ef1abe81
nicola giannotta <nicolagiannotta84@gmail.com>
2022-11-22 17:58:37 +0000

    project set-up

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,12 @@
-import '../styles/app.css';
+import React from "react";
+import "../styles/app.css";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Surreal Estate</h2>
     </div>
   );
-}
+};
 
 export default App;
